153d698a0a53d8473cd5d4292cd6d92245facab4
Abe Augustijn <abe.augustijn@hotmail.nl>
2020-03-12 17:38:16 +0100

    Fix: dmenu as default

--- config.def.h
+++ config.def.h
@@ -63,8 +63,8 @@ static const char *roficmd[] = { "rofi", "-show", "run", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_space,  spawn,          {.v = roficmd } },
-	{ MODKEY|ShiftMask,             XK_space,  spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_space,  spawn,          {.v = roficmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
